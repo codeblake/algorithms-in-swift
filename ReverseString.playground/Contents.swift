9e02db85a180c1e76cdbfc6bc34829168404d408
codeblake <codeblake@outlook.com>
2021-05-07 10:03:16 +0100

    Added reverse string functions

---
+++ ReverseString.playground/Contents.swift
@@ -0,0 +1,23 @@
+//extension StringProtocol {
+//	/// Subscript with integers
+//	subscript(offset: Int) -> Character {
+//		self[index(startIndex, offsetBy: offset)]
+//	}
+//}
+
+func reverseStringIterativly(_ string: String) -> String {
+	var newString = ""
+	for c in string {
+		newString = String(c) + newString
+	}
+	return newString
+}
+
+func reverseStringRecursively(_ string: String) -> String {
+	if string.count == 1 { return string }
+	let substring = String(string.suffix(string.count-1))
+	return reverseStringRecursively(substring) + "\(string.first!)"
+}
+
+reverseStringIterativly("hello")
+reverseStringRecursively("hello")
